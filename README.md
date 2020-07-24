6eda7654801e51f47f816076b1326892316765b6
Vladimir <scherbinin.vladimir@gmail.com>
2020-07-24 14:50:03 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# yvkurse
+# выгрузка статей из https://vk.com/@yvkurse в .csv файл
+
+## Установка
+```
+git clone https://github.com/scherbininvladimir/yvkurse
+pip3 install -r requirements.txt
+```
+
+## Настройка
+```
+В файле settings.py:
+output_file - файл, в который будут выведены статьи
+```
+
+## Запуск
+```
+python3 get_csv.py
+```
--- get_csv.py
+++ get_csv.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 
 import settings
 
+
 def get_csv_string(link):
     site = settings.url.split('@')[0]
-    r = requests.get(site + link, headers=settings.headers)
+    r = requests.get(site + link)
     soup = BeautifulSoup(r.text, "html.parser")
     article_block = soup.find("div", class_="article_view")
     article_title = ""
@@ -13,12 +14,10 @@ def get_csv_string(link):
     article_images = ""
     if article_block:
         article_title = article_block.find("h1").text.strip()
-        images = [image.get("src") for image in article_block.find_all("img")]
         text_blocks = [p for p in article_block.find_all("p")]
-    
         for paragraph in text_blocks:
             article_text = article_text + paragraph.getText()
-    
+        images = [image.get("src") for image in article_block.find_all("img")]
         for image in images:
             if article_images:
                 article_images = article_images + ", "
@@ -28,14 +27,18 @@ def get_csv_string(link):
 
 r = requests.get(settings.url, headers=settings.headers)
 soup = BeautifulSoup(r.text, "html.parser")
-block_main_article_link = soup.find("div", class_="author_page_block_main_article").find('a').get('href')
+main_article_block = soup.find("div", class_="author_page_block")
+main_article_block_link = ""
+if main_article_block:
+    main_article_block_link = main_article_block.find('a').get('href')
 article_links = [link.find("a").get("href") for link in soup.find_all("div", class_="author_page_grid_article")]
 
 with open(settings.output_file, "w") as file:
-    print(get_csv_string(block_main_article_link), file=file, sep="\n")
-    for link in article_links:
-        print(get_csv_string(link), file=file, sep="\n")
-        
-   
-
-
+    if main_article_block_link:
+        print("'title', 'text', 'images'", file=file, sep="\n")
+        print(get_csv_string(main_article_block_link), file=file, sep="\n")
+        for link in article_links:
+            print(get_csv_string(link), file=file, sep="\n")
+        print(f'Статьи выгружены в {settings.output_file}')
+    else:
+        print("Статьи не найдены")
